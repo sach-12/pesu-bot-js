bce9168b451d9070858327c1c9ac5d80c0b19bc9
Sachin Shankar <sachinshankar2010@gmail.com>
2022-01-01 00:30:54 +0530

    added few eventws

--- src/client-commands/misc.js
+++ src/client-commands/misc.js
@@ -10,6 +10,25 @@ class Misc {
     sleep = (seconds) => 
         new Promise(resolve => setTimeout(resolve, seconds*1000))
 
+    deverify = async(mid) => {
+        const mongoose = require('mongoose')
+        const {verified} = require('./models')
+
+        mongoose.connect('mongodb://localhost:27017/pesu',
+        {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        });
+
+        const ret = await verified.findOneAndDelete({ID: mid})
+        if(ret === null) {
+            return false
+        }
+        else {
+            return true
+        }
+    }
+
     
 }
 const misc = new Misc()
