744c2fea720053c426f0bffb083d53bb2c25d6d7
Sachin Shankar <sachinshankar2010@gmail.com>
2022-01-16 14:15:08 +0530

    code for ban this anon conext menu interaction

--- src/client/interactions/cmenu.js
+++ src/client/interactions/cmenu.js
@@ -1,6 +1,7 @@
 // Handles all button interactions
 
-const { Collection } = require("discord.js");
+const { Collection, MessageEmbed } = require("discord.js");
+const config = require('../../config.json')
 
 class ContextMenu {
 
@@ -10,7 +11,54 @@ class ContextMenu {
     }
 
     anonban = async(interaction) => {
-        await interaction.reply({content: "Interaction still under development...", ephemeral: true})
+        await interaction.deferReply({ephemeral: true})
+
+        // Interaction allowed only for admin/mods
+        if(interaction.member.roles.cache.some((role) => [config.admin, config.mod].includes(role.id))) {
+            
+            // Get the author ID
+            const slash = require('./slash')
+            const messageId = interaction.targetMessage.id
+            const authorId = Object.keys(slash.anonCache).find(key => slash.anonCache[key].includes(messageId))
+            if(authorId === undefined) {
+                await interaction.editReply({content: "This wasn't an anon message only da what you doing?"})
+            }
+            else {
+                // Since this interaction cannot be supplied with any parameters, default reason is provided
+                const reason = "No reason specified"
+                const {anonbanHelper} = require('../helpers/misc')
+
+                // Anon-ban the user, returns false if he/she was already banned
+                const banStatus = await anonbanHelper(authorId, reason)
+                if(banStatus === false) {
+                    await interaction.editReply({content: "Dude's already banned from anon messaging"})
+                }
+                else {
+                    // Send embed DM to banned anon
+                    const target = await interaction.guild.members.fetch(authorId)
+                    const banEmbed = new MessageEmbed({
+                        title: "Notification",
+                        color: "RED",
+                        timestamp: Date.now(),
+                        description: "You have been banned from using anon messaging"
+                    })
+                        .addField("Reason", reason, false)
+                        .addField("Message link", interaction.targetMessage.url, false);
+                    
+                    await interaction.editReply({content: `Member has been banned from anon messaging\nReason: ${reason}`})
+                    try {
+                        await target.send({embeds: [banEmbed]})
+                    } catch (error) {
+                        if(error instanceof DiscordAPIError) {
+                            await interaction.followUp({content: "DMs were closed"})
+                        }
+                    }
+                }
+            }
+        }
+        else {
+            await interaction.editReply({content: "You are not authorised to use this"})
+        }
     }
 }
 
